be3661ec64bd8210326ea4ccf5dea6e21b75bb8f
harshapericherla <harsha.pericherla@gmail.com>
2019-12-12 23:56:35 -0500

    added keys.js file

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleClientId: '224936282751-6cfjlsf8olcngnev469pi29hagpmg70t.apps.googleusercontent.com',
+    googleClientSecret: '686fjVhQjscdULTHvoQnkCyg',
+    mongoURI: 'mongodb+srv://harsha:harsha@cluster0-ksebv.mongodb.net/emaily?retryWrites=true&w=majority'
+};
