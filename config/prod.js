18ed3fa195f77c958119f2220bebe8eee678772e
harshapericherla <harsha.pericherla@gmail.com>
2019-12-29 23:58:06 -0500

    added support for heroku proxy

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,3 @@
-/*
-module.exports = {
-    googleClientId: '876935226747-c99sf0ferumscj2vukconuuovacncg8b.apps.googleusercontent.com',
-    googleClientSecret: 'NGJEWoQyHxFOmctScbf-1rnu',
-    mongoURI: 'mongodb+srv://user1:ilI80EiajsyFYfBv@cluster0-fanfm.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey:'akuhaKIHSKHkidhkppnkGkuGvlnsbkuhkau'
-};
-*/
 
 module.exports = {
     googleClientId: process.env.GOOGLE_CLIENT_ID,
